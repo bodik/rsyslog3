4817b65db7b8a25df5d2bb5a2c43f986aa882e05
root <root@ztook3.meta.zcu.cz>
2017-10-07 23:37:36 +0200

    rediser basic testers

---
+++ rediser/tests/perf_redis_reader.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+
+import argparse
+import logging
+import os
+import redis
+import signal
+import sys
+import time
+import threading
+
+logger = logging.getLogger()
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s '+os.path.basename(sys.argv[0])+'[%(process)d] %(levelname)s %(message)s')
+
+
+def reporter():
+	global count
+	while True:
+		logger.debug("report %d" % count)
+		time.sleep(args.report)
+
+
+def teardown(signum, frame):
+	global count
+	logger.info("RESULT: read %d" % count)
+	sys.exit(0)
+
+
+def reader():
+	global count
+
+	while True:
+		r = redis.StrictRedis(host=args.host, port=args.port)
+		while True:
+			if args.batch > 1:
+				pipe = r.pipeline()
+				for i in xrange(args.batch):
+					pipe.lpop(args.key)
+				result = pipe.execute()
+				for i in result:
+					if i:
+						count += 1
+			else:
+				print r.blpop(args.key)
+
+
+if __name__ == "__main__":
+	count = 0
+
+        parser = argparse.ArgumentParser()
+	parser.add_argument("--host", default="localhost", help="redis sert host")
+	parser.add_argument("--port", default=16379, type=int, help="redis server port")
+	parser.add_argument("--key", default="test", help="redis key to read")
+	parser.add_argument("--report", default=1, type=int, help="test id")
+	parser.add_argument("--batch", default=100, type=int, help="batch size for reading pipeline")
+        args = parser.parse_args()
+
+	signal.signal(signal.SIGTERM, teardown)
+	signal.signal(signal.SIGINT, teardown)
+	try:
+		reporter = threading.Thread(target=reporter)
+		reporter.setDaemon(True)
+		reporter.start()
+
+		reader()
+	except Exception as e:
+		logger.error(e)
+		teardown(None, None)
+	
+	teardown(None, None)
+
---
+++ rediser/tests/perf_rediser_writer.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+
+import argparse
+import logging
+import os
+import signal
+import socket
+import sys
+import time
+import threading
+
+logger = logging.getLogger()
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s '+os.path.basename(sys.argv[0])+'[%(process)d] %(levelname)s %(message)s')
+
+
+def reporter():
+	global count
+	while True:
+		logger.debug("report %d" % count)
+		time.sleep(args.report)
+
+
+def teardown(signum, frame):
+	global count
+	logger.info("RESULT: written %d" % count)
+	sys.exit(0)
+
+
+def writer():
+	global count
+	
+	f = open("/dev/urandom" ,"r")
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((args.host, args.port))
+
+	i = args.batch
+	while ( i != 0 ):
+		data = "".join(["{:02x}".format(ord(x)) for x in  f.read(100)])
+		s.send("perftestmessage \\n %s tmsg%d %s\n" % (args.id, count, str(data)))
+		i -= 1
+		count += 1
+
+	s.close()
+	f.close()
+
+if __name__ == "__main__":
+	count = 0
+
+        parser = argparse.ArgumentParser()
+	parser.add_argument("--host", default="localhost", help="rediser server host")
+	parser.add_argument("--port", default=47800, type=int, help="rediser server port")
+	parser.add_argument("--report", default=1, type=int, help="test id")
+	parser.add_argument("--id", default="auto", help="test id")
+	parser.add_argument("--batch", default=100, type=int, help="number of messages to write; -1 is infinite")
+        args = parser.parse_args()
+
+	signal.signal(signal.SIGTERM, teardown)
+	signal.signal(signal.SIGINT, teardown)
+	try:
+		reporter = threading.Thread(target=reporter)
+		reporter.setDaemon(True)
+		reporter.start()
+
+		writer()
+	except Exception as e:
+		logger.error(e)
+		teardown(None, None)
+
+	teardown(None, None)
