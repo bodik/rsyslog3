a9bfe49f7404a7b8158429cd34f8e96bade1bfe4
root <root@ztook1.meta.zcu.cz>
2017-10-11 10:32:03 +0200

    doc update

--- rsyslog/bin/deb-make.sh
+++ rsyslog/bin/deb-make.sh
@@ -4,13 +4,10 @@ cd /tmp || exit 1
 
 if [ ! -d collab-maint-rsyslog ]; then
 
-	# repozitar vytvoreny clonem z collabu
-	# git://anonscm.debian.org/collab-maint/rsyslog.git
-	# musi byt navic nastaveny jako 'git config remote.origin.fetch 'refs/heads/*:refs/heads/*''
-	# aby se do nej daly natahovat nove veci z upstreamu upstreamu 'git fetch --all'
-	# klasicky post update hook
-	# po fetchi se navic musi rucne dat 'git update-server-info' protoze na fetch neni hook
-	#git clone http://home.zcu.cz/~bodik/meta/git/collab-maint-rsyslog
+	# source repository cloned from collab git://anonscm.debian.org/collab-maint/rsyslog.git
+	# ?? set for fetch from origin 'git config remote.origin.fetch 'refs/heads/*:refs/heads/*'' to be able to fetch from upstream 'git fetch --all'
+	# after each fetch 'git update-server-info' run
+	# enable hooks/post-update for https
 	git clone http://esb.metacentrum.cz/collab-maint-rsyslog.git
 
 	cd collab-maint-rsyslog
