3731f641181726949a884d5dfb0ffbedc22931d0
root <root@ztook1.meta.zcu.cz>
2017-10-11 10:32:50 +0200

    reading params

--- rsyslog/bin/dir-pack.sh
+++ rsyslog/bin/dir-pack.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-if [ -z $1 ]; then
-        echo "ERROR: no dir"
-        exit 1
-fi
+rreturn() { echo "$2"; exit $1; }
 
-DIR=$1
+test -n "$1" || rreturn 1 "ERROR: missing DIR"
+DIR="$1"
 
 for all in $(find $DIR -type f ! -name "*7z"); do
         nice -n 15 7za a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on $all.7z $all
--- rsyslog/bin/dir-push-es-modulesdata.sh
+++ rsyslog/bin/dir-push-es-modulesdata.sh
@@ -1,28 +1,28 @@
+#!/bin/sh
 
-if [ -z $1 ]; then
-	echo "ERROR: no dir"
-	exit 1
-fi
-DIR=$1
+SERVER="esb.metacentrum.cz"
+PORT=47801
 
-if [ -z $2 ]; then
-	DEST=esb.metacentrum.cz
-else
-	DEST=$2
-fi
+rreturn() { echo "$2"; exit $1; }
+usage() { echo "Usage: $0 -d <DIR> [-s <SERVER>] [-p <PORT>]" 1>&2; exit 1; }
+while getopts "d:s:p:" o; do
+	case "${o}" in
+        	d) DIR=${OPTARG} ;;
+        	s) SERVER=${OPTARG} ;;
+		p) PORT=${OPTARG} ;;
+		*) usage ;;
+	esac
+done
+shift "$(($OPTIND-1))"
+test -n "$DIR" || rreturn 1 "ERROR: missing DIR"
 
-if [ -z $3 ]; then
-	PORT=49558
-else
-	PORT=$3
-fi
 
 echo -n "INFO: du "
 du -sh $DIR
 BEGIN=`date +%s`
 echo "BEGIN: $DIR"
 
-(find $DIR -type f ! -name "*7z" -exec cat {} \; ; find $DIR -type f -name "*7z" -exec 7z -so x {} 2>/dev/null \;) | grep "modules:" |pv | nc -q3 $DEST $PORT
+(find $DIR -type f ! -name "*7z" -exec cat {} \; ; find $DIR -type f -name "*7z" -exec 7z -so x {} 2>/dev/null \;) | grep "modules:" | pv | nc -q3 $SERVER $PORT
 
 END=`date +%s`
 TIME=$(($END-$BEGIN))
