0e8b1b68d58efbed2b82d575e6e5d8a23bf237aa
root <root@debian.cesnet.cz>
2017-10-16 09:26:51 +0200

    parallel all exec

--- rsyslog/bin/metacloud_allclients_config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-. /puppet/metalib/bin/lib.sh
-
-test -n "$1" || rreturn 1 "$0 missing forward_type"
-
-metacloud.init all 'RC-' "pa.sh -e 'class { \"rsyslog::client\": forward_type=>\"$1\"}'"
