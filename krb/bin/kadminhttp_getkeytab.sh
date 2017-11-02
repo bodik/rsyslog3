3b19df44b5bd958c0e008a6e0e829d666b1fec40
root <root@ztook1.meta.zcu.cz>
2017-11-02 08:32:56 +0100

    error handling

--- krb/bin/kadminhttp_getkeytab.sh
+++ krb/bin/kadminhttp_getkeytab.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-curl --output /etc/krb5.keytab "http://$(/puppet/metalib/bin/avahi_findservice.sh _kdc._udp):47900/get_keytab"
+set -e
+curl --fail --output /etc/krb5.keytab "http://$(/puppet/metalib/bin/avahi_findservice.sh _kdc._udp):47900/get_keytab"
 chmod 600 /etc/krb5.keytab
