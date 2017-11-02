56e312dc0acdb95267e21ebdd5fb6a3b3fa65fda
root <root@ztook1.meta.zcu.cz>
2017-11-02 15:12:18 +0100

    heimdal uses inetd so we'll use it too

--- krb/manifests/remctladm.pp
+++ krb/manifests/remctladm.pp
@@ -7,29 +7,6 @@ class krb::remctladm() {
 	package { ["remctl-server", "remctl-client"]:
 		ensure => installed
 	}
-
-	service { "inetd": }
-	file_line { "remove inetd remctl conf":
-		ensure => absent,
-		path => "/etc/inetd.conf", line => "/usr/sbin/remctld", match => "/usr/sbin/remctld",
-		match_for_absence => true,
-		notify => Service["inetd"],
-	}
-
-	file { "/etc/systemd/system/remctld.service": 
-		source => "puppet:///modules/${module_name}/etc/systemd/system/remctld.service",
-		owner => "root", group => "root", mode => "0644",
-		require => Package["remctl-server"],
-	}
-	service { "remctld":
-		ensure => running,
-		enable => true,
-		require => File["/etc/systemd/system/remctld.service"],
-	}
-
-
-
-	# client
 	file { "/etc/remctl/conf.d/remctladm":
 		content => "remctladmd ALL /puppet/krb/files/remctladm/remctladmd.py /etc/remctl/acl/remctladmd\n",
 		owner => "root", group => "root", mode => "0640",
@@ -37,7 +14,7 @@ class krb::remctladm() {
 		notify => Service["remctld"],
 	}
 	file { "/etc/remctl/acl/remctladmd":
-		content => "host/$fqdn@RSYSLOG3\n",
+		content => "anyuser:auth\n",
 		owner => "root", group => "root", mode => "0640",
 		require => Package["remctl-server"],
 		notify => Service["remctld"],
@@ -62,5 +39,4 @@ class krb::remctladm() {
 			line => "host/${fqdn} cpw,list,delete,modify,add,get,get-keys",
 		}
 	}
-
 }
