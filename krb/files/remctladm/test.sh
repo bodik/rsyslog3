5c306dafce6fe438c61bf9ed2fbac83fab6204ce
root <root@ztook1.meta.zcu.cz>
2017-11-02 16:36:53 +0100

    insall and test tuning

--- krb/files/remctladm/test.sh
+++ krb/files/remctladm/test.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-find $(dirname $(readlink -f $0)) -type f -name 'test_*' -exec /bin/sh {} \;
+set -e
+
+for all in $(find $(dirname $(readlink -f $0)) -type f -name 'test_*'); do 
+	sh $all
+done
