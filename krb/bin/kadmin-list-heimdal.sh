9a65e8d5b1bb3b99b77eefc1d32fe1f858623ea6
root <root@ztook1.meta.zcu.cz>
2018-07-23 12:31:59 +0200

    realm selector

--- krb/bin/kadmin-list-heimdal.sh
+++ krb/bin/kadmin-list-heimdal.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-kadmin.heimdal --config=/etc/heimdal-kdc/kdc.conf --local list -s --column-info=principal,kvno,keytypes '*'
+
+if [ -z $1 ]; then
+	REALM=$(cat /etc/krb5.conf  | grep default_realm | awk '{print $3}')
+else
+	REALM=$1
+fi
+
+kadmin.heimdal --config=/etc/heimdal-kdc/kdc.conf --local --realm=$REALM list -s --column-info=principal,kvno,keytypes '*'
