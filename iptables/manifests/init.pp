e42c2c5ec0b3c2e45b0dd3039135aa4dcb9c0ca4
root <root@ztook1.meta.zcu.cz>
2018-09-25 14:26:05 +0200

    refactoring
    * removed sshcrack.pl, no ipv6 support, fail2ban redundant
    
    * rules selection simplification, site/infrastructure specific settings will
      not be part of module code but rather links or separate files in modules data
    
    * main script refactoring

--- iptables/manifests/init.pp
+++ iptables/manifests/init.pp
@@ -17,43 +17,36 @@ class iptables (
 	notice("INFO: pa.sh -v --noop --show_diff -e \"include ${name}\"")
 
 
-	package { ["iptables"]: ensure => installed }
-	package { "iptables-persistent": ensure => absent,}
+
+	package { "iptables": ensure => installed }
+	package { ["netfilter-persistent", "iptables-persistent"]: ensure => absent, }
 
 	file { ["/var/lib/iptables", "/var/lib/ip6tables"]:
 		ensure => "directory",
 		owner => "root", group => "root", mode => "0750",
 	}
 
-
-	case $fqdn {
-		'somehoney.cesnet..cz':
-			{ $rules_v4_real = "puppet:///modules/${module_name}/rules.v4.somehoney" }
-
-		default:	
-			{ $rules_v4_real = [ $rules_v4, "puppet:///modules/${module_name}/PRIVATEFILE_rules.v4.${fqdn}", "puppet:///modules/${module_name}/rules.v4.${fqdn}", "puppet:///modules/${module_name}/rules.v4"] }
-	}
-	case $fqdn {
-		'somehoney.cesnet..cz': 	
-			{ $rules_v6_real = "puppet:///modules/${module_name}/rules.v6.somehoney" }
-
-		default:
-			{ $rules_v6_real = [ $rules_v6, "puppet:///modules/${module_name}/PRIVATEFILE_rules.v6.${fqdn}", "puppet:///modules/${module_name}/rules.v6.${fqdn}", "puppet:///modules/${module_name}/rules.v6"] }
-	}
-
-
 	file { "/var/lib/iptables/active":
-		source => $rules_v4_real,
+		source => [
+			$rules_v4,
+			"puppet:///modules/${module_name}/PRIVATEFILE_rules.v4.${fqdn}",
+			"puppet:///modules/${module_name}/rules.v4.${fqdn}",
+			"puppet:///modules/${module_name}/rules.v4"],
 		owner => "root", group => "root", mode => "0640",
 		require => File["/var/lib/iptables"],
 		notify => Service["iptables"],
 	}
 	file { "/var/lib/ip6tables/active":
-		source => $rules_v6_real,
+		source => [
+			$rules_v6,
+			"puppet:///modules/${module_name}/PRIVATEFILE_rules.v6.${fqdn}",
+			"puppet:///modules/${module_name}/rules.v6.${fqdn}",
+			"puppet:///modules/${module_name}/rules.v6"],
 		owner => "root", group => "root", mode => "0640",
 		require => File["/var/lib/ip6tables"],
 		notify => Service["ip6tables"],
 	}
+
 	file { "/var/lib/iptables/inactive":
 		source => "puppet:///modules/${module_name}/rules.v4-inactive",
 		owner => "root", group => "root", mode => "0640",
@@ -67,7 +60,6 @@ class iptables (
 		notify => Service["ip6tables"],
 	}
 
-
 	file { "/etc/init.d/iptables":
 		source => "puppet:///modules/${module_name}/etc/init.d/iptables",
 		owner => "root", group => "root", mode => "0755",
@@ -78,7 +70,8 @@ class iptables (
 		owner => "root", group => "root", mode => "0755",
 		notify => Exec["systemctl daemon-reload"],
 	}
-	ensure_resource( 'exec', "systemctl daemon-reload", { "command" => '/bin/systemctl daemon-reload', refreshonly => true} )
+	ensure_resource("exec", "systemctl daemon-reload", { "command" => "/bin/systemctl daemon-reload", refreshonly => true})
+
 	service { "iptables": 
 		ensure => running,
 		enable => true, 
@@ -88,19 +81,5 @@ class iptables (
 		ensure => running,
 		enable => true, 
 		require => [File["/etc/init.d/ip6tables"], File["/var/lib/ip6tables/active"], Exec["systemctl daemon-reload"]],
-	}
-
-
-
-
-	file { "/usr/local/bin/sshcrack.pl":
-		source => "puppet:///modules/${module_name}/usr/local/bin/sshcrack.pl",
-		owner => "root", group => "root", mode => "0755",
-	}
-	cron { "sshcrack":
-		command => "/usr/local/bin/sshcrack.pl",
-		user    => root,
-		hour    => "*",
-		minute  => "*/5"
 	}
 }
